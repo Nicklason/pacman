9268f8dca718d916839412b2f40ef7f0c369a478
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2018-11-20 15:17:55 +0100

    Tegn vægge

--- viewer.py
+++ viewer.py
@@ -38,9 +38,32 @@ class Viewer():
         pygame.display.flip()
 
     def draw_game(self):
+        def draw_wall_line(start, end):
+            startx, starty = start
+            endx, endy = end
+
+            pygame.draw.line(self.screen, (0, 0, 100), (startx-1, starty-1), (endx-1, endy-1))
+
+        self.screen.fill((0, 0, 0))
         for x in range(0, self.game.map.width):
             for y in range(0, self.game.map.height):
                 thing = self.game.map.coordinates[x][y]
 
-                if thing is 2:
+                if thing is 1:
+                    # Tjek om der er en mur til alle fire sider
+                    above = False if -1 < y-1 < self.game.map.height and self.game.map.coordinates[x][y-1] is not 1 else True
+                    below = False if -1 < y+1 < self.game.map.height and self.game.map.coordinates[x][y+1] is not 1 else True
+                    left = False if -1 < x-1 < self.game.map.width and self.game.map.coordinates[x-1][y] is not 1 else True
+                    right = False if -1 < x+1 < self.game.map.width and self.game.map.coordinates[x+1][y] is not 1 else True
+
+                    if above is False:
+                        draw_wall_line((x*16, y*16), (x*16+16, y*16))
+                        #pygame.draw.line(self.screen, (0, 0, 100), (x*16, y*16), (x*16+16, y*16), 1)
+                    if below is False:
+                        draw_wall_line((x*16, y*16+16), (x*16+16, y*16+16))
+                    if left is False:
+                        draw_wall_line((x*16, y*16), (x*16, y*16+16))
+                    if right is False:
+                        draw_wall_line((x*16+16, y*16), (x*16+16, y*16+16))
+                elif thing is 2:
                     pygame.draw.rect(self.screen, (255, 255, 0), pygame.Rect(x*16+7, y*16+7, 2, 2))
